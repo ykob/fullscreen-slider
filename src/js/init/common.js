1bb7d1bc489d817baae6fcd22f369fc076462c65
yoichi kobayashi <info@tplh.net>
2019-04-19 11:16:01 +0900

    changed the way to attach the hover effect.

--- src/js/init/common.js
+++ src/js/init/common.js
@@ -1,2 +1,9 @@
+import Hover from 'js-util/Hover';
+
 export default function() {
+  const elmHover = document.querySelectorAll('.js-hover');
+
+  elmHover.forEach(elm => {
+    new Hover(elm);
+  })
 };
